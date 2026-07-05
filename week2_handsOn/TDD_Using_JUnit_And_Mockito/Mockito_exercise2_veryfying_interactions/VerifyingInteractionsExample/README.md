e0b6b0bd784fa4f0db434e8646a687b35baae9ed
Keshav Bhawsar <keshavbhawsar11@gmail.com>
2026-07-05 21:50:07 +0530

    Mockito Exercise 3 Completed- Verifying Interactions

---
+++ week2_handsOn/TDD_Using_JUnit_And_Mockito/Mockito_exercise2_veryfying_interactions/VerifyingInteractionsExample/README.md
@@ -0,0 +1,22 @@
+# Exercise 2 - Verifying Interactions
+
+## Objective
+
+Verify that a mocked object's method is called using Mockito.
+
+## Files
+
+- ExternalApi.java
+- MyService.java
+- MyServiceTest.java
+
+## Technologies Used
+
+- Java
+- Maven
+- JUnit 5
+- Mockito
+
+## Result
+
+Successfully verified that the mocked API method was invoked.
---
+++ week2_handsOn/TDD_Using_JUnit_And_Mockito/Mockito_exercise2_veryfying_interactions/VerifyingInteractionsExample/src/test/java/MyServiceTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.*;
+
+public class MyServiceTest {
+
+    @Test
+    public void testVerifyInteraction() {
+
+        // Create Mock Object
+        ExternalApi mockApi = mock(ExternalApi.class);
+
+        // Inject Mock
+        MyService service = new MyService(mockApi);
+
+        // Call Method
+        service.fetchData();
+
+        // Verify Interaction
+        verify(mockApi).getData();
+    }
+}
